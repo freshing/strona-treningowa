21d64dc96fd48f9912c6b9c1502dab1099578a90
Dariusz Faruga <dariusz.faruga@jazzy.pro>
2017-07-17 12:19:09 +0200

    Updated content and contact part

--- src/modules/content.js
+++ src/modules/content.js
@@ -7,12 +7,12 @@ module.exports = class Content extends React.Component {
     return (
       <div className="content">
         <div className="content_container">
-          <div>
+          <div className="content_question">
             Dlaczego warto
             skorzystać z naszych
             usług?
           </div>
-          <div>
+          <div className="content_description">
             Lorem ipsum dolor sit amet.
           </div>
           <div>
@@ -20,14 +20,23 @@ module.exports = class Content extends React.Component {
           </div>
         </div>
         <div className="content_container">
-          <div className="content_element">1 Poufoność powierzonych informacji</div>
-          <div className="content_element">2 Wrażliwość i pełne zrozumienie Twojej sytuacji</div>
-          <div className="content_element">3 Bezpłatność porad</div>
-          <div className="content_element">4 Bezstronność doradców i nieocenianie klienta</div>
-          <div className="content_element">5 Niezależność i kierowanie się Twoim dobrem</div>
-          <div className="content_element">6 Samodzielność klienta w podejmowaniu ostatecznych decyzji</div>
-          <div className="content_element">7 Otwartość na każdą osobę potrzebującą porady</div>
-          <div className="content_element">8 Aktualność i rzetelność informacji na temat obowiązujących przepisów oraz oferty instytucji i organizacji działających na rzecz obywateli</div>
+          <div className="content_element"><div className="content_element--number">1</div>
+          <div className="content_element--text">Poufoność powierzonych informacji</div></div>
+          <div className="content_element"><div className="content_element--number">2</div>
+          <div className="content_element--text">Wrażliwość i pełne zrozumienie Twojej sytuacji</div></div>
+          <div className="content_element"><div className="content_element--number">3</div>
+          <div className="content_element--text">Bezpłatność porad</div></div>
+          <div className="content_element"><div className="content_element--number">4</div>
+          <span className="content_element--text">Bezstronność doradców i nieocenianie klienta</span></div>
+          <div className="content_element"><div className="content_element--number">5</div>
+          <span className="content_element--text">Niezależność i kierowanie się Twoim dobrem</span></div>
+          <div className="content_element"><div className="content_element--number">6</div>
+          <span className="content_element--text">Samodzielność klienta w podejmowaniu ostatecznych decyzji</span></div>
+          <div className="content_element"><div className="content_element--number">7</div>
+          <span className="content_element--text">Otwartość na każdą osobę potrzebującą porady</span></div>
+          <div className="content_element"><div className="content_element--number">8</div>
+          <span className="content_element--text">Aktualność i rzetelność informacji na temat obowiązujących przepisów oraz oferty instytucji
+          i organizacji działających na rzecz obywateli</span></div>
         </div>
       </div>
     )
