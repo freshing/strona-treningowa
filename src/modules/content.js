4ea897349626f2ada3832044b212607fc82bbda7
Dariusz Faruga <dariusz.faruga@jazzy.pro>
2017-07-18 11:16:03 +0200

    Added package

--- src/modules/content.js
+++ src/modules/content.js
@@ -6,36 +6,38 @@ module.exports = class Content extends React.Component {
     return (
       <div className="content">
         <div className="content_container">
-          <div className="content_question">
-            Dlaczego warto
-            skorzystać z naszych
-            usług?
+          <div className="content_column">
+            <div className="content_question">
+              Dlaczego warto
+              skorzystać z naszych
+              usług?
+            </div>
+            <div className="content_description">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div>
+              <a href="#" onClick={this.props.onClick}><div className="button_contact button_contact--dark">SKONTAKTUJ SIĘ Z NAMI</div></a>
+            </div>
           </div>
-          <div className="content_description">
-            Lorem ipsum dolor sit amet.
+          <div className="content_column">
+            <div className="content_element"><div className="content_element--number">1</div>
+            <div className="content_element--text">Poufoność powierzonych informacji</div></div>
+            <div className="content_element"><div className="content_element--number">2</div>
+            <div className="content_element--text">Wrażliwość i pełne zrozumienie Twojej sytuacji</div></div>
+            <div className="content_element"><div className="content_element--number">3</div>
+            <div className="content_element--text">Bezpłatność porad</div></div>
+            <div className="content_element"><div className="content_element--number">4</div>
+            <span className="content_element--text">Bezstronność doradców i nieocenianie klienta</span></div>
+            <div className="content_element"><div className="content_element--number">5</div>
+            <span className="content_element--text">Niezależność i kierowanie się Twoim dobrem</span></div>
+            <div className="content_element"><div className="content_element--number">6</div>
+            <span className="content_element--text">Samodzielność klienta w podejmowaniu ostatecznych decyzji</span></div>
+            <div className="content_element"><div className="content_element--number">7</div>
+            <span className="content_element--text">Otwartość na każdą osobę potrzebującą porady</span></div>
+            <div className="content_element"><div className="content_element--number">8</div>
+            <span className="content_element--text">Aktualność i rzetelność informacji na temat obowiązujących przepisów oraz oferty instytucji
+            i organizacji działających na rzecz obywateli</span></div>
           </div>
-          <div>
-            <a href="#" onClick={this.props.onClick}><div className="button_contact button_contact--dark">SKONTAKTUJ SIĘ Z NAMI</div></a>
-          </div>
-        </div>
-        <div className="content_container">
-          <div className="content_element"><div className="content_element--number">1</div>
-          <div className="content_element--text">Poufoność powierzonych informacji</div></div>
-          <div className="content_element"><div className="content_element--number">2</div>
-          <div className="content_element--text">Wrażliwość i pełne zrozumienie Twojej sytuacji</div></div>
-          <div className="content_element"><div className="content_element--number">3</div>
-          <div className="content_element--text">Bezpłatność porad</div></div>
-          <div className="content_element"><div className="content_element--number">4</div>
-          <span className="content_element--text">Bezstronność doradców i nieocenianie klienta</span></div>
-          <div className="content_element"><div className="content_element--number">5</div>
-          <span className="content_element--text">Niezależność i kierowanie się Twoim dobrem</span></div>
-          <div className="content_element"><div className="content_element--number">6</div>
-          <span className="content_element--text">Samodzielność klienta w podejmowaniu ostatecznych decyzji</span></div>
-          <div className="content_element"><div className="content_element--number">7</div>
-          <span className="content_element--text">Otwartość na każdą osobę potrzebującą porady</span></div>
-          <div className="content_element"><div className="content_element--number">8</div>
-          <span className="content_element--text">Aktualność i rzetelność informacji na temat obowiązujących przepisów oraz oferty instytucji
-          i organizacji działających na rzecz obywateli</span></div>
         </div>
       </div>
     )
